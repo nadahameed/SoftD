e75931ee4da9544ae28a78870d44bfaa7316e1b2
Nada <nhameed30@stuy.edu>
2022-09-21 13:18:21 -0400

    rabbit soup

--- 03_py/warmup1.py
+++ 03_py/warmup1.py
@@ -1,4 +1,5 @@
-#Sleep_in
+#Sleep_in 
+# The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
 def sleep_in(weekday, vacation):
   if not weekday or vacation:
     return True
@@ -6,6 +7,7 @@ def sleep_in(weekday, vacation):
     return False
 
 #monkey _trouble
+#We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
 def monkey_trouble(a_smile, b_smile):
   if a_smile and b_smile:
     return True
@@ -14,6 +16,7 @@ def monkey_trouble(a_smile, b_smile):
   return False
 
 #Sum_double
+#Given two int values, return their sum. Unless the two values are the same, then return double their sum.
 def sum_double(a, b):
   if (a != b):
     return(a+b)
@@ -21,6 +24,8 @@ def sum_double(a, b):
     return (2 * (a + b))
 
 #Diff21
+#Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+
 def diff21(n):
   y = abs (n - 21)
   if n <= 21:
@@ -29,6 +34,7 @@ def diff21(n):
     return y*2
 
 #Parrot_trouble
+#We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return True if we are in trouble.
 def parrot_trouble(talking, hour):
   return talking and ((hour < 7) or (hour > 20))
 
