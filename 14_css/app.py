c654fbf11f3280be77d02534859d75a5bb0a1659
Nada <nhameed30@stuy.edu>
2022-10-19 12:42:10 -0400

    haha

---
+++ 14_css/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)    #create Flask object
+
+@app.route("/")
+def webpage():
+    return render_template('index.html')
+
+if __name__ == "__main__": #false if this file imported as module
+#enable debugging, auto-restarting of server when this file is modified
+app.debug = True 
+app.run()
